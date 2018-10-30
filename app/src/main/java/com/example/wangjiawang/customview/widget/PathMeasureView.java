0d753a94239aed9921fd562a0ddf40d689a8f392
wangjiawang <15829348578@163.com>
2018-10-30 20:39:08 +0800

    自定义View进阶-PathMeasure的使用2(绕圆的箭头)

--- app/src/main/java/com/example/wangjiawang/customview/widget/PathMeasureView.java
+++ app/src/main/java/com/example/wangjiawang/customview/widget/PathMeasureView.java
@@ -1,15 +1,23 @@
 package com.example.wangjiawang.customview.widget;
 
+
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
 import android.graphics.Canvas;
-import android.graphics.Color;
+import android.graphics.Matrix;
+
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PathMeasure;
 import android.util.AttributeSet;
+
 import android.util.Log;
 import android.view.View;
+
+import com.example.wangjiawang.customview.R;
 
 /**
  * Created by wangjiawang on 2018/10/30
@@ -20,66 +28,67 @@ import android.view.View;
  */
 public class PathMeasureView extends View {
     private Paint mPaint;
-    private float mCenterX;
-    private float mCenterY;
-
+    private float centerX,centerY;
+    private float currentValue = 0; //用于记录当前的位置，取值范围【0，1】映射Path的整个长度
+    private float[] pos;            //当前点的实际位置
+    private float[] tan;            //当前点的tangent值，用于计算图片所需旋转的角度
+    private Bitmap mBitmap;         //箭头图片
+    private Matrix mMatrix;         //矩阵，用于对图片进行一些操作
     public PathMeasureView(Context context) {
         super(context);
     }
 
     public PathMeasureView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init(context);
+    }
+
+    private void init(Context context) {
         mPaint = new Paint();
-        mPaint.setColor(Color.BLACK);
         mPaint.setStyle(Paint.Style.STROKE);
-        mPaint.setTextSize(60);
+
+        pos = new float[2];
+        tan = new float[2];
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = 8;       //缩放图片
+        mBitmap = BitmapFactory.decodeResource(context.getResources(),R.drawable.arrow,options);
+        mMatrix = new Matrix();
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        mCenterX = w/2;
-        mCenterY = h/2;
+        centerX = w/2;
+        centerY = h/2;
     }
 
     @Override
     @SuppressLint("DrawAllocation")
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        //绘制辅助线
-        drawAuxiliaryLine(canvas);
-
-        canvas.translate(mCenterX,mCenterY);
-
-        Path path = new Path();
-        path.addRect(-100,-100,100,100,Path.Direction.CW);
-        canvas.drawPath(path,mPaint);
-        path.addRect(-200,-200,200,200,Path.Direction.CW);
-
-        mPaint.setColor(Color.BLACK);
-        mPaint.setStrokeWidth(4);
-        canvas.drawPath(path,mPaint);
-
+        canvas.translate(centerX,centerY);  //平移坐标系
+        Path path = new Path();     //创建path
+        path.addCircle(0,0,200,Path.Direction.CW);
         PathMeasure measure = new PathMeasure(path,false);
-        float length1 = measure.getLength();
-        measure.nextContour();
-        float length2 = measure.getLength();
+        currentValue += 0.005;
+        if(currentValue >= 1) {
+            currentValue = 0;
+        }
+        //方法1
+//        measure.getPosTan(measure.getLength()* currentValue,pos,tan);   //获取坐标与趋势
+//
+//        mMatrix.reset();
+//        float degrees = (float) (Math.atan2(tan[1],tan[0])*180/Math.PI);        //获取角度,Math.atan2:获取角度大小，单位为弧度。
+//
+//        mMatrix.postRotate(degrees,mBitmap.getWidth()/2,mBitmap.getHeight() /2);    //旋转图片
+//        mMatrix.postTranslate(pos[0] - mBitmap.getWidth()/2,pos[1] - mBitmap.getHeight()/2);    //将图片绘制中心与当前坐标重叠
+        //方法2
+        measure.getMatrix(measure.getLength()* currentValue,mMatrix,PathMeasure.POSITION_MATRIX_FLAG|PathMeasure.TANGENT_MATRIX_FLAG);
+        mMatrix.preTranslate(-mBitmap.getWidth()/2,-mBitmap.getHeight()/2);
 
-        Log.d("TAG","第一条路径长度为："+length1);
-        Log.d("TAG","第二条路径长度为："+length2);
+        canvas.drawPath(path,mPaint);
+        canvas.drawBitmap(mBitmap,mMatrix,mPaint);
 
-
-    }
-
-    private void drawAuxiliaryLine(Canvas canvas) {
-
-        canvas.save();
-
-        canvas.translate(mCenterX,mCenterY);
-        mPaint.setColor(Color.RED);
-        canvas.drawLine(-2000,0,2000,0,mPaint);
-        canvas.drawLine(0,-2000,0,2000,mPaint);
-
-        canvas.restore();
+        invalidate();
     }
 }
